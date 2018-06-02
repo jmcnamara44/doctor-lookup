a5f71cf2d46a71f187f1faea68c3cabd3c36f543
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-02 12:00:42 -0700

    only show doctors that having matching names to the user's search, not other doctors working for the same practice.

--- src/main.js
+++ src/main.js
@@ -23,12 +23,14 @@ $(document).ready(function() {
 
     const getElements = function(response) {
       $(".results").text(`Below is a list of doctor's meeting your search criteria:`);
-
+      $(".results").append(`<tr><th>Doctor Name</th><th>Accepts New Patients</th><th>Address</th><th>Phone Number</th><th>Website</th></tr>`)
       response.data.forEach(function(doctorData) {
-        doctorData.practices.forEach(function(practice) {
-          $(".results").append(`<tr><td>${practice.name}</td><td>${practice.accepts_new_patients}</td><td>${practice.visit_address.street} ${practice.visit_address.city}, ${practice.visit_address.state_long} ${practice.visit_address.zip}</td><td>${practice.phones[0].number}</td><td>${practice.website}</td></tr>`)
+        $(".results").append(`<tr><td>${doctorData.profile.last_name}, ${doctorData.profile.first_name}</td><td>${doctorData.practices[0].accepts_new_patients}</td><td>${doctorData.practices[0].visit_address.street} ${doctorData.practices[0].visit_address.city}, ${doctorData.practices[0].visit_address.state_long} ${doctorData.practices[0].visit_address.zip}</td><td>${doctorData.practices[0].phones[0].number}</td><td>${doctorData.practices[0].website}</td></tr>`)
 
-        });
+        // doctorData.practices.forEach(function(practice) {
+        //   $(".results").append(`<tr><td>${practice.name}</td><td>${practice.accepts_new_patients}</td><td>${practice.visit_address.street} ${practice.visit_address.city}, ${practice.visit_address.state_long} ${practice.visit_address.zip}</td><td>${practice.phones[0].number}</td><td>${practice.website}</td></tr>`)
+        //
+        // });
       });
     }
   });
