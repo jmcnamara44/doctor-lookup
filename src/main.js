887b86662406ee0a5352723a92c6c72e9076a180
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-02 13:09:28 -0700

    try to update the css and index files to give the table better spacing, not getting the connection to work though.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $("#search").submit(function(event) {
     event.preventDefault();
     let doctorName = $("#doctor-search").val();
-    // let conditionName = $("#condition-search").val();
+    let conditionName = $("#condition-search").val();
 
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${conditionName}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.API_KEY}`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -27,6 +27,15 @@ $(document).ready(function() {
         $(".results").text(`Below is a list of doctor's meeting your search criteria:`);
         $(".results").append(`<tr><th>Doctor Name</th><th>Accepts New Patients</th><th>Address</th><th>Phone Number</th><th>Website</th></tr>`)
         response.data.forEach(function(doctorData) {
+          // console.log(doctorData.practices[0].website);
+          // let websiteInfo = function() {  trying to make it so that when a website comes back undefined the return value can be adjusted.  not able to get the function to work
+          //   if ((doctorData.practices[0].website) != "") {
+          //     return "";
+          //   } else {
+          //     return doctorData.practices[0].website;
+          //   }
+          // };
+          // console.log(websiteInfo);
           $(".results").append(`<tr><td>${doctorData.profile.last_name}, ${doctorData.profile.first_name}</td><td>${doctorData.practices[0].accepts_new_patients}</td><td>${doctorData.practices[0].visit_address.street} ${doctorData.practices[0].visit_address.city}, ${doctorData.practices[0].visit_address.state_long} ${doctorData.practices[0].visit_address.zip}</td><td>${doctorData.practices[0].phones[0].number}</td><td>${doctorData.practices[0].website}</td></tr>`)
         });
       } else {
