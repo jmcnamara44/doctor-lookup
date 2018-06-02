1bfc55d3806b46feb6aba90c70bafd22a7de541e
Jimmy McNamara <jmcnamara44@gmail.com>
2018-06-02 12:07:54 -0700

    give the user a different message if there search returns no doctors, as well as if there is an error with the search.

--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
         let response = JSON.parse(this.responseText);
         getElements(response);
       } else if (this.readyState === 4 && this.status !== 200) {
-        $(".results").text("error");
+        $(".results").text("An error occured while trying to search for the data you requested, please contact the web administrator for this page.");
       } else {
-        $(".results").text("loading");
+        $(".results").text("loading...");
       }
     }
 
@@ -22,16 +22,16 @@ $(document).ready(function() {
     request.send();
 
     const getElements = function(response) {
-      $(".results").text(`Below is a list of doctor's meeting your search criteria:`);
-      $(".results").append(`<tr><th>Doctor Name</th><th>Accepts New Patients</th><th>Address</th><th>Phone Number</th><th>Website</th></tr>`)
-      response.data.forEach(function(doctorData) {
-        $(".results").append(`<tr><td>${doctorData.profile.last_name}, ${doctorData.profile.first_name}</td><td>${doctorData.practices[0].accepts_new_patients}</td><td>${doctorData.practices[0].visit_address.street} ${doctorData.practices[0].visit_address.city}, ${doctorData.practices[0].visit_address.state_long} ${doctorData.practices[0].visit_address.zip}</td><td>${doctorData.practices[0].phones[0].number}</td><td>${doctorData.practices[0].website}</td></tr>`)
 
-        // doctorData.practices.forEach(function(practice) {
-        //   $(".results").append(`<tr><td>${practice.name}</td><td>${practice.accepts_new_patients}</td><td>${practice.visit_address.street} ${practice.visit_address.city}, ${practice.visit_address.state_long} ${practice.visit_address.zip}</td><td>${practice.phones[0].number}</td><td>${practice.website}</td></tr>`)
-        //
-        // });
-      });
-    }
+      if (response.data != "") {
+        $(".results").text(`Below is a list of doctor's meeting your search criteria:`);
+        $(".results").append(`<tr><th>Doctor Name</th><th>Accepts New Patients</th><th>Address</th><th>Phone Number</th><th>Website</th></tr>`)
+        response.data.forEach(function(doctorData) {
+          $(".results").append(`<tr><td>${doctorData.profile.last_name}, ${doctorData.profile.first_name}</td><td>${doctorData.practices[0].accepts_new_patients}</td><td>${doctorData.practices[0].visit_address.street} ${doctorData.practices[0].visit_address.city}, ${doctorData.practices[0].visit_address.state_long} ${doctorData.practices[0].visit_address.zip}</td><td>${doctorData.practices[0].phones[0].number}</td><td>${doctorData.practices[0].website}</td></tr>`)
+        });
+      } else {
+        $(".results").text("There were no doctors matching your search criteria.")
+      }
+    };
   });
 });
